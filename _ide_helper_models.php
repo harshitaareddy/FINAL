4aece650e1589d3dfe9afee80d5b9c38601823cb
harshitaareddy <hk389@njit.edu>
2018-05-06 02:55:01 -0400

    commited to run heroku

---
+++ _ide_helper_models.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Answer
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $question_id
+ * @property string $body
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\Question $question
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Answer whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Answer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Answer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Answer whereQuestionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Answer whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Answer whereUserId($value)
+ * @mixin \Eloquent
+ */
+	class Answer extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Profile
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string|null $fname
+ * @property string|null $lname
+ * @property string|null $body
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereFname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereLname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Profile whereUserId($value)
+ * @mixin \Eloquent
+ */
+	class Profile extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\Question
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $body
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Answer[] $answers
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Question whereUserId($value)
+ * @mixin \Eloquent
+ */
+	class Question extends \Eloquent {}
+}
+
--- routes/web.php
+++ routes/web.php
@@ -43,4 +43,3 @@ Route::resources([
 
 
 
-
